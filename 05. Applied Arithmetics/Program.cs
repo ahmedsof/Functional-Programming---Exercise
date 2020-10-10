52c353bc86e357a920bfb2fd25cc5b91b8e4b0d6
Ahmed Softev <ahmed_v@abv.bg>
2020-10-11 00:34:41 +0300

    C#

---
+++ 05. Applied Arithmetics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int> addFunc = x => x + 1;
+            Func<int, int> multiplayFunc = x => x * 2;
+            Func<int, int> subtractFunc = x => x - 1;
+
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case"add":
+                        numbers = numbers.Select(addFunc).ToList();
+                        break;
+                    case"multyplay":
+                        numbers = numbers.Select(multiplayFunc).ToList();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(subtractFunc).ToList();
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(" ",numbers));
+                        break;
+                    
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 08. Custom Comparator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Array.Sort(numbers, (x, y)=>
+            {
+
+                int sorter = 0;
+                if (x % 2 == 0 && y %2 != 0)
+                {
+                    sorter = -1;
+                }
+
+                else if(x%2 != 0&& y%2==0)
+                {
+                    sorter = 1;
+                }
+                else
+                {
+                    sorter = x - y;
+                }
+                return sorter;
+            });
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
