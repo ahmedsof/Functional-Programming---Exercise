0e041b6e169bb6075c93bc83833d543f633b148c
Ahmed Softev <ahmed_v@abv.bg>
2020-10-11 00:22:58 +0300

    C#

---
+++ 03. Custom Min Function/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> minValue = nums =>
+            {
+                int minNum = int.MaxValue;
+                foreach (int num in nums)
+                {
+                    if (num < minNum)
+                    {
+                        minNum = num;
+
+                    }
+                    
+                }
+                return minNum;
+            };
+            Console.WriteLine(minValue(numbers));
+        }
+        static int GetMin(int[] nums)
+        {
+            int minNum = int.MaxValue;
+            foreach (int num in nums)
+            {
+                if (num < minNum)
+                {
+                    minNum = num;
+
+                }
+                
+            }
+            return minNum;
+        }
+    }
+}
